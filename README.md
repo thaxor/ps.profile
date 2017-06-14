d2c6fa3db81f80d0bfe75fc1796fadc934393921
thaxor <lukehansen@gmail.com>
2017-06-13 19:51:07 -0700

    initial checkin

---
+++ README.md
@@ -0,0 +1,7 @@
+# ps-profile
+
+## Getting Started
+1. `git clone https://github.com/CarBar/ps-profile.git`
+1. Open your profile `notepad $PROFILE`
+1. Add `. \<profile repo location>\profile.ps1`
+1. Reload your profile `. $PROFILE`
---
+++ profile.ps1
@@ -0,0 +1,89 @@
+function prompt {
+  $realLASTEXITCODE = $LASTEXITCODE
+
+  Write-Host
+
+  # Reset color, which can be messed up by Enable-GitColors
+  #$Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+
+  if (Test-Administrator) {
+    # Use different username if elevated
+    Write-Host "(Elevated) " -NoNewline -ForegroundColor White
+  }
+
+  Write-Host "$ENV:USERNAME@" -NoNewline -ForegroundColor DarkYellow
+  Write-Host "$ENV:COMPUTERNAME" -NoNewline -ForegroundColor Magenta
+
+  if ($s -ne $null) {
+    # color for PSSessions
+    Write-Host " (`$s: " -NoNewline -ForegroundColor DarkGray
+    Write-Host "$($s.Name)" -NoNewline -ForegroundColor Yellow
+    Write-Host ") " -NoNewline -ForegroundColor DarkGray
+  }
+
+  #Write-Host " : " -NoNewline -ForegroundColor DarkGray
+  Write-Host $($(Get-Location) -replace ($env:USERPROFILE).Replace('\', '\\'), "~") -NoNewline -ForegroundColor Green
+  Write-Host " : " -NoNewline -ForegroundColor DarkGray
+
+  $global:LASTEXITCODE = $realLASTEXITCODE
+
+  Write-VcsStatus
+
+  Write-Host ""
+
+  return "> "
+}
+
+#Install-Module posh-docker -Scope CurrentUser
+
+function Test-Administrator {
+  $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+  (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+function Start-Administrator {
+  Start-Process powershell -verb RunAs
+}
+
+#aliases
+Set-Alias elevate Start-Administrator #starts a new session as admin, as close to sudo you can get.
+Set-Alias code code-insiders.cmd #aliases the insiders version of code cli
+
+if (Test-Administrator) {
+  (Get-Host).UI.RawUI.BackgroundColor = "DarkRed"
+  Clear-Host
+  Write-Host "Warning: PowerShell is running as an Administrator.`n"
+}
+#else 
+#{
+#    (Get-Host).UI.RawUI.BackgroundColor = 'Black'
+#    (Get-Host).UI.RawUI.ForegroundColor = 'Green'
+#    Clear-Host
+#}
+Write-Host "Calculating the ultimate answer to life, the universe and everything..... Plz wait..."  
+
+if (Test-Path $env:LOCALAPPDATA\GitHub\) {
+  . $env:LOCALAPPDATA\GitHub\shell.ps1
+  . $env:github_posh_git\profile.example.ps1
+}
+
+$env:Path = "$env:Path;.bin" 
+
+if (Test-Path c:\src) {
+  Set-Location c:\src
+}
+
+function NukeDockerStuff() {
+  docker rm $(docker ps -aq) -f
+  docker volumes prune
+  Clear-Host
+}
+
+
+Write-Output "Answer: 42"
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
